737572c23e7a0aa2938491a462be07a1495ec2ad
sochun98 <sochun98@gmail.com>
2024-05-27 20:00:25 +0900

    [feat] Generics basic 1
    - Todo Create, List

---
+++ todo/apis.py
@@ -0,0 +1,76 @@
+from rest_framework import status
+from rest_framework import generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import TodoSerializer
+from .models import Todo
+
+class TodoCreateAPI(APIView):
+    
+    def post(self, request):
+        serializer = TodoSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        todo = serializer.save()
+        return Response(TodoSerializer(todo).data, status=status.HTTP_201_CREATED)
+
+class TodoListAPI(APIView):
+    
+    def get(self, reuqest):
+        todos = Todo.objects.all()
+        serializer = TodoSerializer(todos, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+class TodoRetrieveAPI(APIView):
+    
+    def get(self, request, pk):
+        try:
+            todo = Todo.objects.get(pk=pk)
+        except Todo.DoesNotExist:
+            return Response({"error": "해당하는 todo가 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+
+class TodoUpdateAPI(APIView):
+    
+    def put(self, request, pk):
+        try:
+            todo = Todo.objects.get(pk=pk)
+        except Todo.DoexNotExist:
+            return Response({"error": "해당하는 todo가 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = TodoSerializer(todo, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        todo = serializer.save()
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+        
+    def patch(self, request, pk):
+        try:
+            todo = Todo.objects.get(pk=pk)
+        except Todo.DoexNotExist:
+            return Response({"error": "해당하는 todo가 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+        serializer = TodoSerializer(todo, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        todo = serializer.save()
+        serializer = TodoSerializer(todo)
+        return Response(serializer.data)
+    
+class TodoDeleteAPI(APIView):
+    
+    def delete(self, request, pk):
+        try:
+            todo = Todo.objects.get(pk=pk)
+        except Todo.DoexNotExist:
+            return Response({"error": "해당하는 todo가 없습니다."}, status=status.HTTP_404_NOT_FOUND)
+        todo.delete()
+        return Response(data={"data": "OK"}, status=status.HTTP_204_NO_CONTENT)
+
+class TodoGenericsCreateAPI(generics.CreateAPIView):
+    serializer_class = TodoSerializer
+    
+class TodoGenericsListAPI(generics.ListAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
+
+class TodoGenericsListCreateAPI(generics.ListCreateAPIView):
+    queryset = Todo.objects.all()
+    serializer_class = TodoSerializer
--- todo/urls.py
+++ todo/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
+from . import apis
 from . import views
 
 
 urlpatterns = [
+    path("generics/", apis.TodoGenericsListCreateAPI.as_view()),
+    path("generics/list/", apis.TodoGenericsListAPI.as_view()),
+    path("create/", apis.TodoCreateAPI.as_view()),
+    path("list/", apis.TodoListAPI.as_view()),
+    path("retrieve/<int:pk>/", apis.TodoRetrieveAPI.as_view()),
+    path("update/<int:pk>/", apis.TodoUpdateAPI.as_view()),
+    path("delete/<int:pk>/", apis.TodoDeleteAPI.as_view()),
     path("list/", views.todo_list),
     path("<int:pk>/", views.todo_detail),
     path("<str:name>/", views.todo_detail_name),
