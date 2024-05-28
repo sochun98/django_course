c52ec7368572ac418a09e3de9ecb568444639b2b
sochun98 <sochun98@gmail.com>
2024-05-28 13:26:07 +0900

    [feat]Generic basic 2

--- todo/urls.py
+++ todo/urls.py
@@ -5,7 +5,11 @@ from . import views
 
 urlpatterns = [
     path("generics/", apis.TodoGenericsListCreateAPI.as_view()),
+    path("generics/create/", apis.TodoGenericsCreateAPI.as_view()),
     path("generics/list/", apis.TodoGenericsListAPI.as_view()),
+    path("generics/retrieve/<int:pk>/", apis.TodoGenericsRetrieveAPI.as_view()),
+    path("generics/update/<int:pk>/", apis.TodoGenericsUpdateAPI.as_view()),
+    path("generics/delete/<int:pk>/", apis.TodoGenericsDeleteAPI.as_view()),
     path("create/", apis.TodoCreateAPI.as_view()),
     path("list/", apis.TodoListAPI.as_view()),
     path("retrieve/<int:pk>/", apis.TodoRetrieveAPI.as_view()),
